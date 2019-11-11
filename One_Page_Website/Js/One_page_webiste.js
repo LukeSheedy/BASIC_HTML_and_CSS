84145224b63e9268112a02502f21e4a744ebdb67
LukeSheedy <56566556+LukeSheedy@users.noreply.github.com>
2019-11-11 12:54:04 -0800

    Fixed bugs
    Fixed bugs that displayed photos and captions

--- One_Page_Website/Js/One_page_webiste.js
+++ One_Page_Website/Js/One_page_webiste.js
@@ -1,11 +1,11 @@
 // Open the Modal
 function openModal() {
-    document.getElementById("myProject").style.display = "block";
+    document.getElementById("myModal").style.display = "block";
 }
   
   // Close the Modal
 function closeModal() {
-    document.getElementById("myProject").style.display = "none";
+    document.getElementById("myModal").style.display = "none";
 }
   
 var slideIndex = 1;
@@ -22,19 +22,19 @@ showSlides(slideIndex = n);
 }
   
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("Test");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
+  }
+  for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
